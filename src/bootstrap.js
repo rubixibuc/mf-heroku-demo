1d8d774ce2be473edb731759fa27a00bc98cd482
Jason Rubenstein <rubenstein.jason.i@gmail.com>
2021-12-25 15:21:43 -0500

    use synchronous webcomponent polyfill

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,4 +1,4 @@
 import "construct-style-sheets-polyfill";
-import "@webcomponents/webcomponentsjs/webcomponents-loader.js";
+import "@webcomponents/webcomponentsjs/webcomponents-bundle.js";
 import "regenerator-runtime/runtime";
 import "./routes";
