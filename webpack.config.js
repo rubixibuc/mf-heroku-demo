dc9a34ead988ef2c8ecae7864c6767c86641286e
Jason Rubenstein <rubenstein.jason.i@gmail.com>
2021-12-25 19:50:59 -0500

    add favicon support

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const deps = require("./package.json").dependencies;
 const ExternalTemplateRemotesPlugin = require("external-remotes-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const HtmlWebpackTagsPlugin = require("html-webpack-tags-plugin");
+const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
 
 module.exports = {
   devServer: {
@@ -87,6 +88,14 @@ module.exports = {
           path: "https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@100;300;400&display=swap",
         },
       ],
+    }),
+    new FaviconsWebpackPlugin({
+      favicons: {
+        background: "#000",
+        theme_color: "#fff",
+      },
+      logo: "src/assets/front-end.png",
+      prefix: "[contenthash]/",
     }),
   ],
   resolve: {
