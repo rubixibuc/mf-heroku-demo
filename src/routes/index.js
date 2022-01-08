5a4666d1a52192936a8632dbf2ce0e60636f13d3
Jason Rubenstein <rubenstein.jason.i@gmail.com>
2022-01-07 23:31:29 -0500

    select root div element instead of body

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ import "./routes.css?style";
 import { Router } from "@vaadin/router";
 
 // noinspection JSIgnoredPromiseFromCall
-new Router(document.body).setRoutes([
+new Router(document.getElementById("root")).setRoutes([
   {
     action: () => import("components/home"),
     component: "mfdemo-home",
