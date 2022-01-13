74a88ba86a91b52c8c47d573800d24fff173f330
Jason Rubenstein <rubenstein.jason.i@gmail.com>
2022-01-13 17:56:20 -0500

    bump @rubixibuc/build-scripts

--- build-scripts.config.js
+++ build-scripts.config.js
@@ -1,33 +1,14 @@
+const exposes = require("exposes.json");
+const links = require("./links.json");
 const deps = require("./package.json").dependencies;
 
 module.exports = {
-  expose: {
-    "./components/home": "components/home",
-  },
+  exposes,
+  links,
   logo: "./src/assets/front-end.png",
   shared: {
     hybrids: deps["hybrids"],
   },
-  tags: [
-    {
-      attributes: {
-        rel: "preconnect",
-      },
-      path: "https://fonts.googleapis.com",
-      type: "css",
-    },
-    {
-      attributes: {
-        rel: "preconnect",
-      },
-      path: "https://fonts.gstatic.com",
-      type: "css",
-    },
-    {
-      path: "https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@100;300;400&display=swap",
-      type: "css",
-    },
-  ],
   title: "MF Demo",
   varName: "mfdemo",
 };
