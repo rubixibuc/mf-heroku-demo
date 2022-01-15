75cb349a7f545bec0af3a740443516c013460f52
Jason Rubenstein <rubenstein.jason.i@gmail.com>
2022-01-15 17:40:59 -0500

    remove console.log()

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -2,8 +2,6 @@ import { define, html } from "hybrids";
 import css from "./home.css";
 import frontend from "assets/front-end.png?file";
 
-console.log(css);
-
 export default define({
   render: () =>
     html`<img alt="front end" src="${frontend}" /><span>
