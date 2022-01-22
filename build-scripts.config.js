46707c51b2e21b3b0f2b5d6545e11424e1a44875
Jason Rubenstein <rubenstein.jason.i@gmail.com>
2022-01-22 09:52:22 -0500

    add site description

--- build-scripts.config.js
+++ build-scripts.config.js
@@ -1,11 +1,13 @@
 const exposes = require("./exposes.json");
 const links = require("./links.json");
+const metas = require("./metas.json");
 const deps = require("./package.json").dependencies;
 
 module.exports = {
   exposes,
   links,
   logo: "src/images/front-end.png",
+  metas,
   shared: {
     hybrids: deps["hybrids"],
   },
